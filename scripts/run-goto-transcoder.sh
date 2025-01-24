e5732656b0f62dd74c28942b646c874c124edb52
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2025-01-24 09:38:15 +0000

    run-goto-transcoder.sh now gets the contract as a parameter

--- scripts/run-goto-transcoder.sh
+++ scripts/run-goto-transcoder.sh
@@ -6,7 +6,7 @@ set -e
 # PARAMETERS #
 ##############
 contract_folder=target/kani_verify_std/target/x86_64-unknown-linux-gnu/debug/deps
-supported_regex=checked_unchecked.*.out
+supported_regex=$1
 unsupported_regex=neg
 
 goto_transcoder_git=https://github.com/rafaelsamenezes/goto-transcoder
